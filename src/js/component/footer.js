fd6b1e70d32f9aaec43ac13666a1ed36d6c511f3
M0RT1M3R1694 <128715892+M0RT1M3R1694@users.noreply.github.com>
2023-08-01 20:01:01 +0000

    commit

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
 	<footer className="footer bg-light mt-auto py-3 text-center">
 		<p className="text-dark">
 			Made with <i className="fa fa-heart text-danger" /> by {" "}
-			<a href="https://github.com/giulianac-git">{"Giulianna Cersosimo"}</a>
+			<a href="https://github.com/M0RT1M3R1694-git">{"Fer Vega"}</a>
 		</p>
 	</footer>
 );
